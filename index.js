a75d9ea992fa715a4e6171fa690c6023b8dfcc1e
Shikhar Pandey <pandeyshikhar07@gmail.com>
2025-06-28 08:20:58 +0530

    ✅ Keploy API Testing Task 1 Submission

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello, world!" });
+});
+
+app.post("/greet", (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `Hello, ${name}` });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
